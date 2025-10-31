44f420dad055cef8dd69983dce124e33f7e0476b
Ioan Zîcu <ioanzicu@MacBook.local>
2025-10-31 22:22:46 +0200

    error configuration

--- src/bin/track.rs
+++ src/bin/track.rs
@@ -1,1 +1,11 @@
-fn main() {}
+use error_stack::{Report, Result, ResultExt};
+use track::{
+    error::{AppError, Suggestion},
+    init,
+};
+
+fn main() -> Result<(), AppError> {
+    init::error_reporting();
+
+    Ok(())
+}
--- src/error.rs
+++ src/error.rs
@@ -1,1 +1,7 @@
 //! Top-level error types
+#[derive(Debug, thiserror::Error)]
+#[error("an application error has occurred")]
+pub struct AppError;
+
+/// A suggetion displayed to the user
+pub struct Suggestion(pub &'static str);
