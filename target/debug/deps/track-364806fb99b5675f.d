dc8360b8fd94b5b0eeea476db234893e33bcca95
Ioan Zîcu <ioanzicu@MacBook.local>
2025-10-31 21:49:44 +0200

    project structure

--- target/debug/deps/track-364806fb99b5675f.d
+++ target/debug/deps/track-364806fb99b5675f.d
@@ -1,5 +1,9 @@
-/Users/ioanzicu/Documents/Rust/ztm-rust/projects/track/target/debug/deps/track-364806fb99b5675f.d: src/lib.rs
+/Users/ioanzicu/Documents/Rust/track/target/debug/deps/track-364806fb99b5675f.d: src/lib.rs src/common.rs src/error.rs src/feature.rs src/init.rs
 
-/Users/ioanzicu/Documents/Rust/ztm-rust/projects/track/target/debug/deps/libtrack-364806fb99b5675f.rmeta: src/lib.rs
+/Users/ioanzicu/Documents/Rust/track/target/debug/deps/libtrack-364806fb99b5675f.rmeta: src/lib.rs src/common.rs src/error.rs src/feature.rs src/init.rs
 
 src/lib.rs:
+src/common.rs:
+src/error.rs:
+src/feature.rs:
+src/init.rs:
--- target/debug/deps/track-36da48035d86d02c.d
+++ target/debug/deps/track-36da48035d86d02c.d
@@ -1,5 +1,9 @@
-/Users/ioanzicu/Documents/Rust/ztm-rust/projects/track/target/debug/deps/track-36da48035d86d02c.d: src/lib.rs
+/Users/ioanzicu/Documents/Rust/track/target/debug/deps/track-36da48035d86d02c.d: src/lib.rs src/common.rs src/error.rs src/feature.rs src/init.rs
 
-/Users/ioanzicu/Documents/Rust/ztm-rust/projects/track/target/debug/deps/libtrack-36da48035d86d02c.rmeta: src/lib.rs
+/Users/ioanzicu/Documents/Rust/track/target/debug/deps/libtrack-36da48035d86d02c.rmeta: src/lib.rs src/common.rs src/error.rs src/feature.rs src/init.rs
 
 src/lib.rs:
+src/common.rs:
+src/error.rs:
+src/feature.rs:
+src/init.rs:
