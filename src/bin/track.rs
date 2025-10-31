26f2f89df3452d830a244afd5e14ff2237e6f143
Ioan Zîcu <ioanzicu@MacBook.local>
2025-10-31 22:32:19 +0200

    tracing configuration

--- src/bin/track.rs
+++ src/bin/track.rs
@@ -1,11 +1,16 @@
 use error_stack::{Report, Result, ResultExt};
+use tracing::{info, instrument, warn};
 use track::{
     error::{AppError, Suggestion},
     init,
 };
 
+// RUST_LOG=warn cargo run
+// RUST_LOG=trace cargo run
+
 fn main() -> Result<(), AppError> {
     init::error_reporting();
+    init::tracing();
 
     Ok(())
 }
