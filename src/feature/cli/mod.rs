e02186f7d44c51ba5fa20b093e9e23ca60d20fb9
Ioan Zîcu <ioanzicu@MacBook.local>
2025-11-01 13:04:27 +0200

    parsing cli ags with clap

--- src/feature/cli/mod.rs
+++ src/feature/cli/mod.rs
@@ -1,19 +1,33 @@
+use clap::{Parser, Subcommand};
 use error_stack::Result;
 #[derive(Debug, thiserror::Error)]
 #[error("a CLI error occured")]
 pub struct CliError;
 
-#[derive(Debug, Clone, Copy)]
+#[derive(Debug, Clone, Copy, Subcommand)]
 pub enum Command {
+    /// Start tracking time
     Start,
-    Stop,
-    Report,
+    // Stop,
+    // Report,
+}
+
+#[derive(Parser, Debug)]
+#[command(version, about, arg_required_else_help(true))]
+pub struct Cli {
+    #[command(subcommand)]
+    pub command: Command,
 }
 
 // no command -> err
 // command -> no error
 
 pub fn run() -> Result<(), CliError> {
-    //
-    todo!()
+    let args: Cli = Cli::parse();
+
+    match args.command {
+        Command::Start => todo!(),
+    }
+
+    Ok(())
 }
