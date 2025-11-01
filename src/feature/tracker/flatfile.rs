0cf674f978fdc8da184e6caba01b40b6ce528302
Ioan Zîcu <ioanzicu@MacBook.local>
2025-11-01 13:40:27 +0200

    starting & stoping the tracker

--- src/feature/tracker/flatfile.rs
+++ src/feature/tracker/flatfile.rs
@@ -5,10 +5,13 @@
 // - "lockfile": tracker is running
 // - "database file": JSON doc
 
-use std::path::PathBuf;
+use error_stack::{Result, ResultExt};
+use std::{fs::OpenOptions, path::PathBuf};
 
 #[derive(Debug, thiserror::Error)]
 #[error("filesystem tracker error")]
+pub struct FlatFileTrackerError;
+
 pub struct FlatFileTracker {
     db: PathBuf,
     lockfile: PathBuf,
@@ -25,27 +28,75 @@ impl FlatFileTracker {
         Self { db, lockfile }
     }
 
-    fn start(&self) -> bool {
-        todo!()
+    fn start(&self) -> Result<(), FlatFileTrackerError> {
+        OpenOptions::new()
+            .write(true)
+            .create_new(true)
+            .open(&self.lockfile)
+            .change_context(FlatFileTrackerError)
+            .attach_printable("unable to create new lockfile when starting trakcer")?;
+
+        Ok(())
     }
 
     fn is_running(&self) -> bool {
-        todo!()
+        self.lockfile.exists()
+    }
+
+    fn stop(&self) -> Result<(), FlatFileTrackerError> {
+        std::fs::remove_file(&self.lockfile)
+            .change_context(FlatFileTrackerError)
+            .attach_printable("unable to delete lockfile")?;
+
+        Ok(())
     }
 }
 
 #[cfg(test)]
 mod tests {
+    use std::process::Child;
+
+    use assert_fs::{TempDir, fixture::ChildPath, prelude::PathChild};
+
     use super::*;
 
+    fn tracking_paths() -> (TempDir, ChildPath, ChildPath) {
+        let temp: TempDir = TempDir::new().unwrap();
+        let db: ChildPath = temp.child("db.json");
+        let lockfile: ChildPath = temp.child("lockfile");
+        (temp, db, lockfile)
+    }
+
+    fn new_flat_file_tracker(db: &ChildPath, lockfile: &ChildPath) -> FlatFileTracker {
+        FlatFileTracker::new(db.to_path_buf(), lockfile.to_path_buf())
+    }
+
     #[test]
-    fn starts_tracking_with_default_tracker() {
+    fn is_running_returns_true_after_starting_tracker() {
+        let (_tempdir, db, lockfile) = tracking_paths();
+
         // Given a default tracker
-        let tracker = FlatFileTracker::new("db.json", "lockfile");
+        let tracker: FlatFileTracker =
+            FlatFileTracker::new(db.to_path_buf(), lockfile.to_path_buf());
 
         // When the tracker is started
-        tracker.start();
+        tracker.start().unwrap();
+
         // Then the tracker is running
         assert!(tracker.is_running());
+    }
+
+    #[test]
+    fn is_running_returns_false_after_stopping_tracker() {
+        // Given a new tracker that is running
+        let (_tempdir, db, lockfile) = tracking_paths();
+        let tracker: FlatFileTracker = new_flat_file_tracker(&db, &lockfile);
+        tracker.start().unwrap();
+
+        // When the tracker is started
+        tracker.stop();
+
+        // Then the tracker is no longer running
+        assert!(!tracker.is_running());
     }
 }
